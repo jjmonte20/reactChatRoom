9eae8ca5dfcac1ebec9c57c92314cf1f13adc755
jjmonte20 <jjmonte20@gmail.com>
2019-02-04 12:38:49 -0600

    able to successfully start server with boilerplate info

---
+++ client/build/precache-manifest.f6d4b8f604251a08e59e239ae1d155ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "42f7d3debbaa1ae704b9",
+    "url": "/static/js/main.42f7d3de.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "42f7d3debbaa1ae704b9",
+    "url": "/static/css/main.1b5bf920.chunk.css"
+  },
+  {
+    "revision": "49ce9b642fc1191dd06fe2fd155c871d",
+    "url": "/index.html"
+  }
+];
